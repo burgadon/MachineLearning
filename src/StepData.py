cb09765c079f05731ce311cadaadf3743171645b
Armin Müller <armin.mueller@student.kit.edu>
2018-11-19 14:31:17 +0100

    Modified StepData. Now it only has 2 parameters: the label and the data-array.

--- src/StepData.py
+++ src/StepData.py
@@ -1,40 +1,26 @@
 # StepData.py
 # Author: Armin Müller
 # Created on 01.11.2018
-# Last Modified on: 02.11.2018
+# Last Modified on: 1.11.2018
 #
 # This file provides a data object to store step data in it
 
-class StepData:
-    def __init__(self, label, accelerationX, accelerationY, accelerationZ):
+class StepData(object):
+    def __init__(self):
         # parameters
-        self.label = label
-        self.accelerationX = accelerationX
-        self.accelerationY = accelerationY
-        self.accelerationZ = accelerationZ
+        self.label = ""
+        self.accelerationData = []
     
     #getter
     def getLabel(self):
         return self.label
     
-    def getAccelerationX(self):
-        return self.accelerationX
-    
-    def getAccelerationY(self):
-        return self.accelerationY
-    
-    def getAccelerationZ(self):
-        return self.accelerationZ
+    def getAccelerationData(self):
+        return self.accelerationData
 
     #setter
     def setLabel(self, newLabel):
         self.label = newLabel
     
-    def setAccelerationX(self, newAccelerationX):
-        self.accelerationX = newAccelerationX
-    
-    def setAccelerationY(self, newAccelerationY):
-        self.accelerationY = newAccelerationY
-    
-    def setAccelerationZ(self, newAccelerationZ):
-        self.accelerationZ = newAccelerationZ
+    def setAccelerationData(self, newAccelerationData):
+        self.accelerationData = newAccelerationData
