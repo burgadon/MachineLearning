3ad670dfc41a76d4d271558fe4a8c58d48611956
Armin Müller <armin.mueller@student.kit.edu>
2018-12-10 14:48:14 +0100

    Created a new branch to gather data by listening on a port. This version doesn't need/use any sort of user interaction.

--- src/MachineLearning.py
+++ src/MachineLearning.py
@@ -8,7 +8,6 @@
 
 import numpy as np
 from src.Parser import Parser
-import sys
 import time
 import os
 import datetime
@@ -25,8 +24,6 @@ class NeuralNetwork:
         self.hiddenSize = 3
         
         #weights
-        #self.W1 = np.random.randn(self.inputSize, self.hiddenSize)  # (9x3) weight matrix from input to hidden layer
-        #self.W2 = np.random.randn(self.hiddenSize, self.outputSize) # (3x1) weight matrix from hidden to output layer
         self.W1 = np.matrix([[10.007343465980664, 5.113687635194334, 10.743692465400681],
         [-5.482072882025064, 1.710683866674025, -11.273259210070421],
         [-6.210177219251583, -4.299305906476425, -7.137855498482551],
@@ -38,8 +35,6 @@ class NeuralNetwork:
         [-3.201236245906119, -5.249431273873157, 0.2093727359358657]])
         self.W2 = np.matrix([[-11.288591202763781], [4.982001257876912], [5.114694548460975]])       
         
-        
-
     # Activation function
     def sigmoid(self, t):
         return 1 / (1 + np.exp(-t))
@@ -66,17 +61,9 @@ class NeuralNetwork:
     
         self.W1 += X.T.dot(self.z2_delta)                               # adjusting first set of weights (input --> hidden)
         self.W2 += self.z2.T.dot(self.o_delta)                          # adjusting second set of weights (hidden --> output)
-
-    def train(self, X, y):
-        o = self.feedForward(X)
-        self.backPropagation(X, y, o)
         
 # ------------------------------------------------------------------------------
 # Methods for usage
-
-    def saveWeights(self):
-        np.savetxt("w1.txt", self.W1, fmt="%s")
-        np.savetxt("w2.txt", self.W2, fmt="%s")
 
     def saveResults(self, resultArray):
         resultPath = "classificationResults.txt"
@@ -87,23 +74,14 @@ class NeuralNetwork:
             appendMode = 'a'    # Append only at the end
         else:
             appendMode = 'w'    # Create a new file
-        
-        #print("RESULTARRAY: " + str(resultArray))
-        
+
         file = open(resultPath, appendMode)
-        #for j in range(len(resultArray)):
+        
         for i in range(len(resultArray)):
-            # For DEBUG
-            print(str(resultArray[i]))
-            
             if int(np.round(resultArray[i] * 10)) != 5:
                 file.write(str("noMove"))
             elif int(np.round(resultArray[i] * 10)) == 5:
-                file.write(str("slowWalk"))
-            #elif int(np.round(resultArray[i] * 10)) <= 10:
-            #    file.write(str("casualWork"))
-            #elif int(np.round(resultArray[i] * 10)) == 3:
-            #    file.write(str("fastWalk"))
+                file.write(str("oneStep"))
             else: 
                 file.write(str("labelPlaceholder"))
             
@@ -118,21 +96,20 @@ class NeuralNetwork:
         date = datetime.datetime.strptime(date, "%Y%m%d")
         new_year_day = datetime.datetime(year=date.year, month=1, day=1)
         return (date - new_year_day).days + 1
-
-    def predictWithPrint(self, inputForPrediction):
-        print("Predicted data based on trained weights:")
-        print("Input: \n" + str(inputForPrediction))
-        print("Output: \n" + str(self.feedForward(inputForPrediction)))
         
     def predictWithoutPrint(self, inputForPrediction):
         return self.feedForward(inputForPrediction)
         
     def getUnclassifiedDataToClassify(self, nonInteractive, unclassifiedDataArray):
+        unclassifedDataParser = Parser()
+        
         if (nonInteractive == True):
-            parsedUnclassifiedData = unclassifiedDataArray
+            unclassifedDataParser.processDataArray(unclassifiedDataArray)
+            
+            # Retrieve data back
+            parsedUnclassifiedData = unclassifedDataParser.getDataArray()
         else:
             unclassifiedDataPath = input("Enter path to file with data, which is to be classified: ")
-            unclassifedDataParser = Parser()
             unclassifedDataParser.setDestination(unclassifiedDataPath)
             unclassifedDataParser.processData()
 
@@ -172,7 +149,7 @@ class NeuralNetwork:
         timeout = 0.25
         ser = serial.Serial(port, baudrate, timeout)
         if ser.isOpen():
-            # print(ser.name + " was successfully opened.")
+            print(ser.name + " was successfully opened.")
             while self.listen == True:
                 rawInputData.append(ser.readline())
         else:
@@ -180,135 +157,23 @@ class NeuralNetwork:
         
         ser.close()
         return rawInputData
-        
-        
-# ------------------------------------------------------------------------------
-# Start of Example
 
-#if __name__ == "__main__": 
-#     # X = (hours studying, hours sleeping), y = score on test, xPredicted = 4 hours studying & 8 hours sleeping (input data for prediction)
-#     X = np.array(([2, 9], [1, 5], [3, 6]), dtype=float)
-#     y = np.array(([92], [86], [89]), dtype=float)
-#     xPredicted = np.array(([4,8]), dtype=float)
-#     
-#     # scale units
-#     X = X / np.amax(X, axis=0) # maximum of X array
-#     xPredicted = xPredicted / np.amax(xPredicted, axis=0) # maximum of xPredicted (our input data for the prediction)
-#     y = y / 100 # max test score is 100
-#     
-#     nn = NeuralNetwork()
-#     
-#     # training the NN 100,000 times
-#     print("Training: Started ...", end=" ")
-#     for i in range(100000):        
-#         if (i % 1000) == 0:
-#             print("# " + str(i) + "\n")
-#             print("Input (scaled): \n" + str(X))
-#             print("Actual Output: \n" + str(y))
-#             print("Predicted Output: \n" + str(nn.feedForward(X)))
-#             print("Loss: \n" + str(np.mean(np.square(y - nn.feedForward(X))))) # mean sum squared loss
-#             print("\n")
-#         nn.train(X, y)
-#     print("DONE!\n", sep=' ', end="", flush=True)
-#     
-#     print("Save weights:", end=" ")
-#     nn.saveWeights()
-#     print("DONE!\n\n", sep=' ', end="", flush=True)
-#     nn.predict()
-    
-# End of example
 # ------------------------------------------------------------------------------
 
 if __name__ == "__main__":
-    # Initialize and run parser
-    parser = Parser()
-    #parser.askForAverageCalculation()           # Asks whether new average values should be calculated
-    
-    # Ask whether an NN should be trained
-    #nnTraining = input("Do you want to train a neural network for step recognition? (y(es) / n(o)): ")
-    #if (nnTraining == "yes") | (nnTraining == "y"):
-#         NN = NeuralNetwork()
-#         
-#         NN.listen = True
-#         #inArray = NN.listenOnPort()
-#         #parser.processDataArray(inArray)
-#         parser.askForDestination()
-#         parser.processData()
-#         
-#         # Retrieve parsed data as an array
-#         parsedData = []
-#         parsedData = parser.getDataArray()
-#         
-#         # Set X (input step data values) and y (input labels corresponding to the step data values)
-#         X = []
-#         y = []
-#         (X, y) = NN.setInputForClassificationScaled(parsedData)
-#         
-#         # New training method
-#         print("Training: Started ...", end=" ")
-#         t = time.process_time()
-#         loss = 1.0
-#         trainingCounter = 0
-#         while (loss > 0.004):
-#             NN.train(X, y)
-#             loss = np.mean(np.square(y - NN.feedForward(X))) # mean sum squared loss
-#             
-#             if (trainingCounter % 10000) == 0:
-#                 print("# " + str(trainingCounter) + "\n")
-#                 print("Input (scaled): \n" + str(X))
-#                 print("Actual Output: \n" + str(y))
-#                 print("Predicted Output: \n" + str(NN.feedForward(X)))
-#                 print("Loss: \n" + str(loss))
-#                 print("\n")
-#             
-#             trainingCounter += 1
-#             
-#         elapsedTime = float(int((time.process_time() - t) * 100)) / 100     # only 2 decimal positions
-#         print("DONE!\n", sep=' ', end="", flush=True)
-#         print("Time for training: " + str(elapsedTime) + " seconds.")
-#         
-#         # Save the weight values in a text-file
-#         print("Save weights:", end=" ")
-#         NN.saveWeights()
-#         print("DONE!\n", sep=' ', end="", flush=True)
-#         print("Final loss: " + str(loss) + "\n")
-#         
-#         # Get data which should be classified
-#         (XTemp, yTemp) = NN.getUnclassifiedDataToClassify(False, "");   # Interactive version
-#         inForPrediction = np.array((XTemp), dtype=float)
-#         inForPrediction /= np.amax(inForPrediction, axis=0)             # maximum of inForPrediction (our input data for the prediction)
-#         
-#         # Predict
-#         result = []
-#         for j in range(len(inForPrediction)):
-#             result.append(NN.predictWithoutPrint(inForPrediction[j]))
-#         #NN.predictWithPrint(inForPrediction)
-#         
-#         # Save results in a file
-#         NN.saveResults(result)
-#         
-# 
-#     elif (nnTraining == "no") | (nnTraining == "n"):
-#         # nothing to do
-#         print("Exiting the program!")
-#         sys.exit(0)
-#     else:
-#         print("Your given answer (" + nnTraining + ") was not recognized. Exiting the program!")
-#         sys.exit(0)
-
-#-------------------------------------------------------------------------------
     NN = NeuralNetwork()
+    
     # Get data which should be classified
-    (XTemp, yTemp) = NN.getUnclassifiedDataToClassify(False, "");   # Interactive version
+    rawDataFromPort = NN.listenOnPort()
+    (XTemp, yTemp) = NN.getUnclassifiedDataToClassify(True, rawDataFromPort);   # non-interactive version
     inForPrediction = np.array((XTemp), dtype=float)
-    inForPrediction /= np.amax(inForPrediction, axis=0)             # maximum of inForPrediction (our input data for the prediction)
+    inForPrediction /= np.amax(inForPrediction, axis=0)                         # maximum of inForPrediction (our input data for the prediction)
      
     # Predict
     result = []
     for j in range(len(inForPrediction)):
         result.append(NN.predictWithoutPrint(inForPrediction[j]))
-    #NN.predictWithPrint(inForPrediction)
-     
+
     # Save results in a file
     NN.saveResults(result)
     
--- src/Parser.py
+++ src/Parser.py
@@ -14,60 +14,6 @@ class Parser(object):
         self.destination = ""
         self.data = []
         self.lineCtr = 1
-        self.average = [0.0, 0.0, 0.0]
-
-    def askForDestination(self):
-        self.destination = input("Type in the destination of the file, which should be parsed:\n")
-        print("Input Destination: " + self.destination)
-
-    def askForAverageCalculation(self):
-        result = input("Do you want to calculate the average for noise reduction purposes? (y(es) / n(o)): ")
-        if (result == "yes") | (result == "y"):
-            # calculate the average for noise reduction
-            self.askForDestination()
-            self.processData()
-            for i in range(len(self.average)):
-                self.average[i] /= 3 * self.lineCtr
-
-            print("The average values are: x = " + str(self.average[0]) + ", y = " + str(self.average[1]) + ", z = " + str(self.average[2]) + ". They were calculated over " + str(self.lineCtr) + " datasets.\n")
-            
-        elif (result == "no") | (result == "n"):
-            # nothing to do
-            return None
-        else:
-            print("Your given answer (" + result + ") was not recognized. Try again!")
-            self.askForAverageCalculation()
-
-        return None
-
-    def processData(self):
-        # Reset processed data array
-        self.data = []
-        
-        with open(self.destination, "r") as file:
-            data = file.readlines()
-            self.lineCtr = 1    # reset line counter
-
-            for line in data:
-                # if line is empty: skip it
-                lineWithoutSpacesAndLineFeeds = line.replace(" ", "").rstrip()
-                if len(lineWithoutSpacesAndLineFeeds) == 0:
-                    continue
-
-                # for noise reduction purposes
-                if line.find("noise") > -1:
-                    self.processNoiseDataLine(line.replace(" ", "").rstrip())
-                    self.lineCtr += 1
-                elif len(line) < 20:
-                # line too short, skip it
-                    continue
-                else:
-                    # normal data processing
-                    stepData = StepData()   # create new step data object
-
-                    self.processLine(line.replace(" ", "").rstrip(), stepData)
-                    self.data.append(stepData)
-                    self.lineCtr += 1
                     
     # If the data is gathered through listening on a port, use this method
     def processDataArray(self, rawDataArray):
@@ -80,11 +26,7 @@ class Parser(object):
             if len(lineWithoutSpacesAndLineFeeds) == 0:
                 continue
 
-            # for noise reduction purposes
-            if line.find("noise") > -1:
-                self.processNoiseDataLine(line.replace(" ", "").rstrip())
-                self.lineCtr += 1
-            elif len(line) < 20:
+            if len(line) < 20:
                 # line too short, skip it
                 continue
             else:
@@ -101,22 +43,6 @@ class Parser(object):
         line = self.processPairOfLeadingAndTrailingBrackets(line)
         line = self.processLabel(line, stepData)
         line = self.processAccelerationData(line, stepData)
-
-#         if not line.isEmpty():
-#             raise ValueError("Error processing line " + str(self.lineCtr)
-#                 + ": The processed line should be empty at this point, but \""
-#                 + line + "\" was found.")
-
-    def processNoiseDataLine(self, line):
-        line = self.processRubbishAtStart(line)
-        line = self.processRubbishAtEnd(line)
-        line = self.processPairOfLeadingAndTrailingBrackets(line)
-        line = self.processNoiseData(line)
-
-#         if len(line) > 0:
-#             raise ValueError("Error processing line " + str(self.lineCtr)
-#                 + ": The processed line should be empty at this point, but \""
-#                 + line + "\" was found.")
 
     def processRubbishAtStart(self, line):
         # check for undefined start characters
@@ -160,11 +86,13 @@ class Parser(object):
                 line = line.replace("[{", "", 1)
                 line = line[:-2]
             else:
-                raise ValueError("Error processing line " + str(self.lineCtr)
+                print("Error processing line " + str(self.lineCtr)
                     + ": No closing brackets (\"}]\") found at end of line.")
+                line = ""
         else:
-            raise ValueError("Error processing line " + str(self.lineCtr)
+            print("Error processing line " + str(self.lineCtr)
                     + ": No opening brackets (\"[{\") found at start of line.")
+            line = ""
 
         return line
 
@@ -228,54 +156,10 @@ class Parser(object):
 
         return line
 
-    def processNoiseData(self, line):
-        # check for the "Label" keyword and delete it
-        if line.startswith("\"Label\""):
-            line = line.replace("\"Label\":", "")
-
-        if line.find("noise") > -1:
-            line = line.replace("noise,", "")
-            line = line.replace("\"Acceleration\":{", "", 3)
-            line = line.replace("}", "", 3)     # delete matching closing brackets
-
-            # replace keywords for axes
-            line = line.replace("\"x-Axes\":", "", 1)
-            line = line.replace(",\"x-Axes\":", ";", 2)
-            line = line.replace(",\"y-Axes\":", ";", 3)
-            line = line.replace(",\"z-Axes\":", ";", 3)
-            line = line.replace(",", "")        # delete trailing comma
-
-            splittedLine = line.split(";")
-
-            self.average[0] += (float(splittedLine[0]) + float(splittedLine[3]) + float(splittedLine[6]))
-            self.average[1] += (float(splittedLine[1]) + float(splittedLine[4]) + float(splittedLine[7]))
-            self.average[2] += (float(splittedLine[2]) + float(splittedLine[5]) + float(splittedLine[8]))
-
-        else:
-            raise ValueError("Error processing line " + str(self.lineCtr)
-                    + ": The \"noise\" keyword was not found.")
-
-        return line
-
     # getter
     def getDataArray(self):
         return self.data
 
-    def getAverage(self):
-        return self.average
-
     # setter
     def setDestination(self, destinationPath):
         self.destination = destinationPath
-
-    # reverse replace (currently unused)
-    def rreplace(self, string, old, new, occurrence):
-        # replaces a char/string from the ending
-        # s = "1232425"
-        # rreplace(s, '2', ' ', 2) leads to "123 4 5"
-        # rreplace(s, '2', ' ', 3) leads to "1 3 4 5"
-        # rreplace(s, '2', ' ', 4) leads to "1 3 4 5"
-        # rreplace(s, '2', ' ', 0) leads to "1232425"
-
-        li = string.rsplit(old, occurrence)
-        return new.join(li)
